3c2133f2e94c99cdcc3febfa8266819e91803aef
tetiana-shanaida <tetiana.shanaida@nixs.com>
2023-05-18 22:52:05 +0300

    Add Task1.cs

---
+++ Task1.cs
@@ -0,0 +1,21 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System.Formats.Asn1;
+namespace Homework_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double a = double.Parse(Console.ReadLine());
+            double b = double.Parse(Console.ReadLine());
+
+            double s = a * b;
+            double p = 2 * (a + b);
+
+            Console.WriteLine($"Area = {s}");
+            Console.WriteLine($"Perimeter = {p}");
+        }
+    }
+    
+}
+
