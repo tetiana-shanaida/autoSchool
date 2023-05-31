3ce4b4504ae1de2f274f5d78cbc4e97e6f660f68
tetiana-shanaida <tetiana.shanaida@nixs.com>
2023-05-31 17:07:54 +0300

    added SetUp and TearDown

--- Module7/Classic_model.cs
+++ Module7/Classic_model.cs
@@ -1,57 +1,71 @@
+
 namespace Module7
 {
     [TestFixture]
     [Category("Classic model")]
     public class Classic_model
     {
+        private string firstString;
+        private string secondString;
+        private List<string> firstAndLastName;
+        private List<string> anotherfirstAndLastName;
+        private List<string> devices;
+        private string laptop;
+        private int a;
+        private int b;
+
         [SetUp]
         public void Setup()
         {
+            firstString = "This is test string";
+            secondString = "This is test string";
+            firstAndLastName = new List<string>();
+            anotherfirstAndLastName = new List<string>();
+            devices = new List<string>();
+            laptop = "laptop";
+            a = 10;
+            b = 5;
+        }
+
+        [TearDown]
+        public void TearDown()
+        {
+            firstString = null;
+            secondString = null;
+            firstAndLastName.Clear();
+            anotherfirstAndLastName.Clear();
+            devices.Clear();
         }
 
         [Test]
         public void AreStringsEqual()
         {
-            string firstString = "This is test string";
-            string secondString = "This is test string";
             StringAssert.AreEqualIgnoringCase(firstString, secondString);
         }
 
         [Test]
         public void AreListOfStringsEqual()
         {
-            List<string> firstAndLastName = new List<string>()
-            {
-                "Jenna Doe",
-                "Alla Doe"
-            };
-            List<string> anotherFirstAndLastName = new List<string>()
-            {
-                "Jenna Doe",
-                "Alla Doe"
-            };
-            CollectionAssert.AreEqual(firstAndLastName, anotherFirstAndLastName);
+            firstAndLastName.Add("Jenna Doe");
+            firstAndLastName.Add("Another Doe");
+            anotherfirstAndLastName.Add("Jenna Doe");
+            anotherfirstAndLastName.Add("Another Doe");
+            CollectionAssert.AreEqual(firstAndLastName, anotherfirstAndLastName);
         }
 
         [Test]
         public void DoListContainsString()
         {
-            List<string> devices = new List<string>()
-            {
-                "desctop",
-                "mobile",
-                "laptop"
-            };
-            string laptop = "laptop";
+            devices.Add("desctop");
+            devices.Add("mobile");
+            devices.Add("laptop");
             Assert.Contains(laptop, devices);
         }
 
         [Test]
         public void IsAMoreThanB()
         {
-            int a = 10;
-            int b = 5;
             Assert.IsTrue(a > b);
         }
-    }
+    }  
 }
--- Module7/Constraint_model.cs
+++ Module7/Constraint_model.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using NUnit.Framework;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -10,53 +11,66 @@ namespace Module7
     [Category("Constraint model")]
     public class Constraint_model
     {
+        private string firstString;
+        private string secondString;
+        private List<string> firstAndLastName;
+        private List<string> anotherfirstAndLastName;
+        private List<string> devices;
+        private string laptop;
+        private int a;
+        private int b;
+
         [SetUp]
         public void Setup()
         {
+            firstString = "This is test string";
+            secondString = "This is test string";
+            firstAndLastName = new List<string>();
+            anotherfirstAndLastName= new List<string>();
+            devices = new List<string>();
+            laptop = "laptop";
+            a = 10;
+            b = 5;
+        }
+
+        [TearDown]
+        public void TearDown()
+        {
+            firstString = null;
+            secondString = null;
+            firstAndLastName.Clear();
+            anotherfirstAndLastName.Clear();
+            devices.Clear();
         }
 
         [Test]
         public void AreStringsEqual()
         {
-            string firstString = "This is test string";
-            string secondString = "This is test string";
             Assert.That(firstString, Is.EqualTo(secondString));
         }
 
         [Test]
         public void AreListOfStringsEqual()
         {
-            List<string> firstAndLastName = new List<string>()
-            {
-                "Jenna Doe",
-                "Another Doe"
-            };
-            List<string> anotherfirstAndLastName = new List<string>()
-            {
-                "Jenna Doe",
-                "Another Doe"
-            };
+            firstAndLastName.Add("Jenna Doe");
+            firstAndLastName.Add("Another Doe");
+            anotherfirstAndLastName.Add("Jenna Doe");
+            anotherfirstAndLastName.Add("Another Doe");
             Assert.That(firstAndLastName, Is.EqualTo(anotherfirstAndLastName));
         }
 
         [Test]
         public void DoListContainsString()
         {
-            List<string> devices = new List<string>()
-            {
-                "desctop",
-                "mobile",
-                "laptop"
-            };
-            string laptop = "laptop";
+            devices.Add("desctop");
+            devices.Add("mobile");
+            devices.Add("laptop");
             Assert.That(devices, Does.Contain(laptop));
         }
 
         [Test]
         public void IsAMoreThanB()
         {
-            int a = 10;
-            int b = 5;
             Assert.That(a > b, Is.True);
         }
     }
