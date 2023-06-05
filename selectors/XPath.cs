c0985a9e217bff85a1e6f6056bf7d8fdccfa5467
tetiana-shanaida <tetiana.shanaida@nixs.com>
2023-06-05 12:01:12 +0300

    adding fixed selectors

--- selectors/XPath.cs
+++ selectors/XPath.cs
@@ -5,19 +5,11 @@ namespace selectors
     public class XPath
     {
         string search = "//input[@title='Search']";
-
-        string testCases = "//a[text()='Test Cases']";       
-        string TestCases = "//li[contains(@class, 'menu-item')]/a[text()='Test Cases']";
-
+        string testCases = "//li[contains(@class, 'menu-item')]/a[text()='Test Cases']";
         string pullDownButton = "//a[@class='pull-down']";
         string filter = "//button[text()='Filter']";
-
-        string javaScript = "//a[text()='JavaScript']";     
-        string JavaScript = "//li[contains(@class, 'cat-item')]/a[text()='JavaScript']";
-
-        string Android = "//h3[text()='Android Quick Start Guide']/parent::a";
-        string android = " //li[contains(@class, 'product_tag-android')]/descendant::h3[text()='Android Quick Start Guide']/..";       
-
+        string JavaScript = "//ul[@class='product-categories']/li[contains(@class, 'cat-item')]/a[text()='JavaScript']";
+        string android = " //ul[contains(@class, 'products')]/li[contains(@class, 'product')]/descendant::h3[text()='Android Quick Start Guide']/..";       
         string twoHundredEighty = " //h3[text()='HTML5 Forms']/following-sibling::span[contains(@class, 'price')]/span";
         string sortByNewness = " //select[@class='orderby']/option[@value='date']";
         string email = "//input[@type='email']";
